c1ceeaf830f5d53d848e2ddbfdc509f3158eed58
nakanoyuki <yukich41@gmail.com>
2025-05-07 08:21:09 +0900

    リクエストボディ

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
